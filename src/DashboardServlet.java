930a72489c43f37e7d21545c6d3ae4770a92827c
marek.dalida <marek.dalida@gmail.com>
2021-03-15 13:00:19 +0100

    logowanie usera

--- src/DashboardServlet.java
+++ src/DashboardServlet.java
@@ -4,11 +4,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "DashboardServlet", urlPatterns = "/DashboardServlet")
 public class DashboardServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("Dashboard servlet");
+        out.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/UserLoginServlet.java
+++ src/UserLoginServlet.java
@@ -1,11 +1,13 @@
 import models.Role;
 import models.User;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
@@ -28,9 +30,27 @@ public class UserLoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
 
-        String n=request.getParameter("username");
-        out.print("User login servlet Welcome "+n);
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        User user;
+
+        RequestDispatcher requestDispatcher;
+
+        if(users.containsKey(username)){
+            if(users.get(username).equals(password)){
+                user = new User(username, password, Role.USER);
+                session.setAttribute("user", user);
+                requestDispatcher = request.getRequestDispatcher("DashboardServlet");
+            } else {
+                requestDispatcher = request.getRequestDispatcher("loginFailure.jsp");
+            }
+        } else {
+            requestDispatcher = request.getRequestDispatcher("loginFailure.jsp");
+        }
+        requestDispatcher.forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
