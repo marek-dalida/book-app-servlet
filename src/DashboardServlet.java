36cf10533662f4fc529adc84617b48b6cce665eb
marek.dalida <marek.dalida@gmail.com>
2021-03-15 16:24:47 +0100

    dodanie library context

--- src/DashboardServlet.java
+++ src/DashboardServlet.java
@@ -1,17 +1,28 @@
+import models.Book;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 @WebServlet(name = "DashboardServlet", urlPatterns = "/DashboardServlet")
 public class DashboardServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
+        ServletContext context = getServletContext();
+        ArrayList<Book> books = (ArrayList<Book>) context.getAttribute("books");
+        System.out.println(books.toString());
+        HttpSession session = request.getSession();
+
         out.print("Dashboard servlet");
+
         out.close();
     }
 
---
+++ src/LibraryContextServletListener.java
@@ -0,0 +1,78 @@
+import models.Book;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import java.util.ArrayList;
+
+@WebListener()
+public class LibraryContextServletListener implements ServletContextListener,
+        HttpSessionListener, HttpSessionAttributeListener {
+
+    // Public constructor is required by servlet spec
+    public LibraryContextServletListener() {
+    }
+
+    // -------------------------------------------------------
+    // ServletContextListener implementation
+    // -------------------------------------------------------
+    public void contextInitialized(ServletContextEvent sce) {
+      /* This method is called when the servlet context is
+         initialized(when the Web application is deployed).
+         You can initialize servlet context related data here.
+      */
+        ArrayList <Book> books = new  ArrayList<>() {{
+            add(new Book("Tytuł_1","Author_1", 2000));
+            add(new Book("Tytuł_2","Author_2", 2000));
+            add(new Book("Tytuł_3","Author_3", 2000));
+            add(new Book("Tytuł_4","Author_4", 2000));
+            add(new Book("Tytuł_5","Author_5", 2000));
+        }} ;
+
+        sce.getServletContext().setAttribute("books", books);
+    }
+
+    public void contextDestroyed(ServletContextEvent sce) {
+      /* This method is invoked when the Servlet Context
+         (the Web application) is undeployed or
+         Application Server shuts down.
+      */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionListener implementation
+    // -------------------------------------------------------
+    public void sessionCreated(HttpSessionEvent se) {
+        /* Session is created. */
+    }
+
+    public void sessionDestroyed(HttpSessionEvent se) {
+        /* Session is destroyed. */
+    }
+
+    // -------------------------------------------------------
+    // HttpSessionAttributeListener implementation
+    // -------------------------------------------------------
+
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is added to a session.
+      */
+    }
+
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+      /* This method is called when an attribute
+         is removed from a session.
+      */
+    }
+
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+      /* This method is invoked when an attribute
+         is replaced in a session.
+      */
+    }
+}
